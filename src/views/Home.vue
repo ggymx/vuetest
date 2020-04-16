924c248b9d4ce1185d29da9b19c08039b97e3722
gegan <2319649532@qq.com>
2020-04-16 16:43:19 +0800

    添加主题icon测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <div class="logo"></div>
     <!-- <HelloWorld :msg="msg" /> -->
     <div class="header">
       <el-button type="primary" id="test" @click="login()">{{$t("login")}}</el-button>
